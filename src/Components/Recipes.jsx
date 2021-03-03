22a799900b5db975a9d43d9ecc79c4a5c0f374c1
CD-Mackay <connor.mackay@gmail.com>
2021-03-03 11:19:18 -0500

    create dropdown menu

--- src/Components/Recipes.jsx
+++ src/Components/Recipes.jsx
@@ -6,7 +6,15 @@ const displayedRecipes = props.recipes.map(recipe => {
 })
   return (
     <div>
+      <form autoComplete='off' onSubmit={event => event.preventDefault()}>
+        <label for="ingredients">What do you have too much of?</label>
+        <select name="ingredients" id="ingredients">
+          <option value="apples">Apples</option>
+          <option value="bananas">Bananas</option>
+          <option value="kale">Kale</option>
+        </select>
     <button onClick={props.showRecipes} >Clickme!</button>
+    </form>
     <div>{displayedRecipes}</div>
     </div>
   )
